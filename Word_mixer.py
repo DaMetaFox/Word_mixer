1806c0319c599f30b05b249dcb3bc898add6520d
Pedro Figueira <70233016+DaMetaFox@users.noreply.github.com>
2022-10-31 22:11:23 +0000

    Create Word_mixer.py

---
+++ Word_mixer.py
@@ -0,0 +1,28 @@
+# define a frase
+frase = ('Little fly, Thy summer’s play My thoughtless hand Has brushed away. Am not I A fly like thee? Or art not thou A man like me?')
+
+# faz a divisao da frase em palavras e cria nova lista words_list
+words_list = [frase.split()]
+
+# Determinar o comprimento da lista nova
+print("** a NOVA lista tem", len(words_list), "elementos **, ou seja: \n", words_list, '\n')
+
+# pesquisar lista 'words_list' e converter palavras com 3 e 7 letras
+
+for item in words_list:
+    if len(item) <= 3:
+        item.lower()
+    print(item)
+#     elif len(item) >= 7:
+#         # y = str(item.upper())
+#         print(item)
+
+#         words_list(word) = words_list(word).lower()
+#     elif len(i) >= 7:
+#       words_list(i).upper
+# print(words_list)
+       
+def word_mixer():
+    words_list_sort = words_list.sort()
+
+word_mixer()
